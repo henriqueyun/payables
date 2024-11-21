d9572a5de0f06daff88f5bbb9f465a34f5c672a8
henriqueyun <henriqueyun@gmail.com>
2024-11-21 06:14:27 -0300

    feat(backend): cors enabled

--- payables-backend/src/main.ts
+++ payables-backend/src/main.ts
@@ -5,6 +5,7 @@ import { json } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(json({ limit: '50mb' }));
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
